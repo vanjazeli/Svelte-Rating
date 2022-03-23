1554bbbb32ecf97ecacd3acf25c375da94d2c4a6
Vanja Želi <92306982+vanjazeli@users.noreply.github.com>
2022-03-23 16:31:10 +0100

    feat: add handleInput to Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,16 +3,30 @@
   import { items, id, tempRating } from "../stores";
 
   let comment = "";
+  let countChecker = true;
 
   const handleSubmit = () => {
     $items = [{ id: $id, rating: $tempRating, text: comment }, ...$items];
     $id++;
     comment = "";
   };
+
+  const handleInput = () => {
+    console.log(comment.length);
+    if (comment.length >= 10) {
+      countChecker = false;
+      console.log(countChecker);
+    } else {
+      countChecker = true;
+      console.log(countChecker);
+    }
+  };
+
+  console.log(countChecker);
 </script>
 
 <div class="header card">
-  <p class="header__message">How would you rate our service?</p>
+  <p class="header__heading">How would you rate our service?</p>
   <form
     class="header__form"
     action="submit"
@@ -25,8 +39,20 @@
         type="text"
         placeholder="Comment..."
         bind:value={comment}
+        on:input={handleInput}
       />
-      <button class="custom-input__button" type="submit">Submit!</button>
+      <button
+        class="custom-input__button"
+        type="submit"
+        disabled={countChecker}
+      >
+        Submit!
+      </button>
     </div>
   </form>
+  {#if countChecker}
+    <p class="header__message">
+      Please enter a message with 10 or more characters.
+    </p>
+  {/if}
 </div>
